d8ee8ee6a00db0a026b58b82de021cc8eaa67da5
PandiMusR <rifqifandimas348@gmail.com>
2024-07-15 20:57:17 +0700

    Change lang to en & add lang option

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEBOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+PREFER_LANG = os.getenv("LANGUAGE")
+if not PREFER_LANG:
+    PREFER_LANG = "English"
 
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
   "top_k": 64,
-  "max_output_tokens": 8192,
+  "max_output_tokens": 4096,
   "response_mime_type": "text/plain",
 }
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel(
   model_name="gemini-1.5-pro",
   generation_config=generation_config,
-  system_instruction="Anda adalah seorang paranormal yang mampu merasakan keberadaan khodam di sekitar benda atau orang. Tugas Anda adalah mendeskripsikan khodam yang mungkin ada, termasuk wujud, sifat, dan energi yang dipancarkan. Sehingga apapun inputnya anggap itu adalah sebuah nama seseorang. Deskripsi tidak harus positif, bisa saja negatif tidak masalah karena ini hiburan. Ini hanya untuk entertaiment jadi tambahkan #hiburan di akhir untuk memberitahu bahwa informasi dari anda hanya hiburan.\nPastikan output dalam plain text dan berbahasa Indonesia.",
+  system_instruction=f"Anda adalah seorang paranormal yang mampu merasakan keberadaan khodam di sekitar benda atau orang. Tugas Anda adalah mendeskripsikan khodam yang mungkin ada, termasuk wujud, sifat, dan energi yang dipancarkan. Sehingga apapun inputnya anggap itu adalah sebuah nama seseorang. Deskripsi tidak harus positif, bisa saja negatif tidak masalah karena ini hiburan. Ini hanya untuk entertaiment jadi tambahkan #hiburan di akhir untuk memberitahu bahwa informasi dari anda hanya hiburan.\nPastikan output dalam plain text dan berbahasa {PREFER_LANG}",
 )
 
 # Bot Telegram Command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Saya adalah bot yang akan membantu Anda mendeskripsikan khodam yang mungkin ada di sekitar Anda. Silakan kirimkan nama seseorang untuk memulai. Apabila ada pertanyaan silahkan mengunjungi github.com/pandimusr atau mengontak Telegram @okabryan')
+    await update.message.reply_text('I am a bot that will help you find out your Spirit "helper", "guardian","protector", "khodam". If you have any questions, please visit github.com/pandimusr or contact Telegram @okabryan')
+    # await update.message.reply_text('Saya adalah bot yang akan membantu Anda mendeskripsikan khodam yang mungkin ada di sekitar Anda. Silakan kirimkan nama seseorang untuk memulai. Apabila ada pertanyaan silahkan mengunjungi github.com/pandimusr atau mengontak Telegram @okabryan')
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Untuk mengetahui detail lebih lanjut tentang bot ini, silahkan bisa mengunjungi github.com/pandimusr atau mengontak @okabryan')
+    await update.message.reply_text('If you have any questions, please visit github.com/pandimusr or contact Telegram @okabryan')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
@@ -42,6 +46,7 @@ def main() -> None:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
